a0d73eab02fea6702c7db49785e51fe6770a13c0
khundakerfaisal <khundakermis@gmail.com>
2025-10-18 22:10:20 +0600

    Added java variable classes

---
+++ src/main/java/Class1/InstanceVariable.java
@@ -0,0 +1,11 @@
+package Class1;
+
+public class InstanceVariable {
+    int age = 25;
+
+    public static void main(String[] args) {
+        InstanceVariable iv = new InstanceVariable();
+        System.out.println("Age is :" + iv.age);
+
+    }
+}
---
+++ src/main/java/Class1/StaticVariable.java
@@ -0,0 +1,11 @@
+package Class1;
+
+public class StaticVariable {
+    static String name = "Atiqur vai";
+
+
+    public static void main(String[] args) {
+        System.out.println("Employee name is:" +name);
+    }
+
+}
