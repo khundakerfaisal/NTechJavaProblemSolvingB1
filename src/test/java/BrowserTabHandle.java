0028563e0a8d5f6a35044069607df53c7909513d
khundakerfaisal <khundakermis@gmail.com>
2025-11-06 17:58:53 +0600

    added tab handle with frameword

--- src/test/java/BrowserTabHandle.java
+++ src/test/java/BrowserTabHandle.java
@@ -1,18 +1,22 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.ArrayList;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+
 public class BrowserTabHandle {
-    public static void main(String[] args) throws InterruptedException {
+
+    @Test
+    public  void tabHandle() throws InterruptedException {
         WebDriver driver=new ChromeDriver();
         //Windows scrolling
         driver.get("https://demoqa.com/browser-windows");
-
         driver.manage().window().maximize();
         driver.findElement(By.id("tabButton")).click();
-
         ArrayList<String> arrayList=new ArrayList<>(driver.getWindowHandles());
         driver.switchTo().window(arrayList.get(1));
         Thread.sleep(1000);
@@ -20,4 +24,5 @@ public class BrowserTabHandle {
 
 
     }
+
 }
